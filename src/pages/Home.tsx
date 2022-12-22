17d5a664efe325a688c0968aa6986dc4364ab2a7
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2022-12-22 21:38:08 +0100

    feat: Initialize home route

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,36 @@
+import { useAppContext } from "../context/appContext";
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  const {
+    search: { results },
+  } = useAppContext();
+
+  const handleCountryClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    cca3: string
+  ): void => {
+    navigate(`/countries/${cca3}`);
+  };
+
+  const countryElements = Object.keys(results).map((cca3) => {
+    const { commonName, population, region, capital } = results[cca3];
+    return (
+      <div
+        key={cca3}
+        style={{ marginBottom: "1rem" }}
+        onClick={(event) => handleCountryClick(event, cca3)}
+      >
+        Name: {commonName}
+        Population: {population}
+        Region: {region}
+        Capital: {capital}
+      </div>
+    );
+  });
+
+  return <div>{countryElements}</div>;
 };
 
 export default Home;
