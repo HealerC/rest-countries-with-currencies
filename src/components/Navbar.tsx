5318374806e16e8d5cdeb0ee6803a74fae342b7b
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2022-12-23 22:36:21 +0100

    feat: Initialize styling of navigation bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,39 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import { useAppContext } from "../context/appContext";
 
 const Navbar = () => {
   const { lightMode, toggleMode } = useAppContext();
 
   return (
-    <div>
-      <h2>Where in the world</h2>
-      <div onClick={toggleMode}>{lightMode ? "Light mode" : "Dark mode"}</div>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Typography component="h1" variant="h6">
+            Where in the world?
+          </Typography>
+          <Button
+            onClick={toggleMode}
+            sx={{
+              border: "1px solid white",
+              color: "white",
+              marginRight: "2rem",
+              textTransform: "capitalize",
+            }}
+            startIcon={
+              lightMode ? <DarkModeOutlinedIcon /> : <LightModeOutlinedIcon />
+            }
+          >
+            {lightMode ? "Dark mode" : "Light mode"}
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
