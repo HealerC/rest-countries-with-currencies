bd0b2567e93455110dc6b745bbb27c372d5d2147
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2022-12-28 00:28:22 +0100

    feat: Set title bar title to go to home route

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,15 +6,22 @@ import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import { useAppContext } from "../context/appContext";
 import ButtonSimple from "./ButtonSimple";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const { lightMode, toggleMode } = useAppContext();
+  const navigate = useNavigate();
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar sx={{ justifyContent: "space-between" }}>
-          <Typography component="h1" variant="h6">
+          <Typography
+            component="h1"
+            variant="h6"
+            onClick={() => navigate("/")}
+            sx={{ cursor: "pointer" }}
+          >
             Where in the world?
           </Typography>
 
