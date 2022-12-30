e21f201ed6c51ef92e8f4bbc3ec7edfaf6813026
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2022-12-31 00:26:29 +0100

    Commenting app interfaces

--- src/utils/interfacesTypes.ts
+++ src/utils/interfacesTypes.ts
@@ -1,3 +1,5 @@
+// All the country data in this app arranged in a map with
+// the key as the country cca3 code
 export type CountryList = {
   [cca3: string]: RestCountriesData;
 };
@@ -12,28 +14,36 @@ export type Region =
   | "Oceania"
   | "Antarctic";
 
+// All the regions plus extra with cca3 values, countries
+// in the regions
 export type RegionList = {
   [key in Region]: string[];
 };
 
+// The state of the app to be distributed by app context
 export type AppState = {
   countryList: CountryList;
   regionList: RegionList;
   lightMode: boolean;
   search: {
+    // Searching the countries
     query: string;
-    filterRegion: Region;
-    results: string[];
+    filterRegion: Region; // Filter the countries by regions
+    results: string[]; // Results of the search (cca3's)
   };
   loading: boolean;
 };
 
+// Currency data of a country. conversionFrom1USD gotten from API
+// used to compute conversion between US $ and country currency
 export type AppCurrencyData = {
   name: string;
   symbol: string;
   conversionFrom1USD: number | null;
 };
 
+// Describing the data received from REST countries API
+// Describing just the data required by the app
 export interface RestCountriesAPI {
   name: {
     common: string;
@@ -67,6 +77,9 @@ export interface RestCountriesAPI {
     svg: string;
   };
 }
+
+// A single country data extending that from the API
+// but with some additional values with some rearrangement/renaming
 export interface RestCountriesData extends RestCountriesAPI {
   commonName: string;
   nativeName: {
@@ -76,6 +89,8 @@ export interface RestCountriesData extends RestCountriesAPI {
     [currency: string]: AppCurrencyData;
   };
 }
+
+// Describing the data received by the API for computing rates
 export interface OpenExchangeAPI {
   base: string;
   rates: {
