0382765345507dabb8cba7a83f104bc0dfad56ab
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2022-12-31 00:11:35 +0100

    Comment utility data builder functions

--- src/utils/apiDataProcessors.ts
+++ src/utils/apiDataProcessors.ts
@@ -6,19 +6,23 @@ import {
 } from "./interfacesTypes";
 import { Region, RegionList } from "./interfacesTypes";
 
+// Process the data from REST countries API
 export const countriesDataProcessor = (data: Object) => {
   return data;
 };
 
+// Process the rates from open exchange rates API
 export const ratesDataProcessor = (data: Object) => {
   return data;
 };
 
+// Describing data from the APIs (parameters to the combined processor)
 export type CountryRates = {
   countries: RestCountriesAPI[];
   rates: OpenExchangeAPI;
 };
 
+// Result from combined processing of both data from API
 type countryRatesDPType = (countryRates: CountryRates) => {
   countryList: CountryList;
   regionList: RegionList;
@@ -35,7 +39,11 @@ export const countriesRatesDataProcessor: countryRatesDPType = ({
   }
 
   let countryList: CountryList = {};
+  // Data we need from each country of the app accessed by its cca3 code
   let regionList: RegionList = {
+    // Each array contains a list of countries with their cca3
+    // Indicates all the countries in the different regions in addition
+    // to All the regions (empty array) and user favorite countries
     All: [],
     Fav: [],
     Africa: [],
@@ -65,6 +73,8 @@ export const countriesRatesDataProcessor: countryRatesDPType = ({
 
     restCountryData.nativeName = {};
     if (name.nativeName) {
+      // Native name of the app will be an object with the key as
+      // the native language and value the native name itself
       Object.keys(name.nativeName).forEach((keyLang) => {
         const value = name.nativeName[keyLang].common;
         restCountryData.nativeName[keyLang] = value;
@@ -80,10 +90,14 @@ export const countriesRatesDataProcessor: countryRatesDPType = ({
     restCountryData.tld = tld;
     restCountryData.flags = flags;
 
+    // The currency of this app is gonna have a name, a symbol
+    // and it's rate conversion from $1 gotten from data from
+    // Open exchange API
     const { currencies } = country;
     let tempCurrencies: {
       [key: string]: AppCurrencyData;
     } = {};
+
     if (currencies) {
       Object.keys(currencies).forEach((currencyCode) => {
         let conversionFrom1USD = rates.rates[currencyCode]
